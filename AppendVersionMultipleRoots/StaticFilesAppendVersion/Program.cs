1d12d31a86556a9abe0fd26e5b6f1bca51c5fd31
Artak Mkrtchyan <artakm@corp.microsoft.com>
2018-03-09 14:50:52 -0800

    Checking in the project

---
+++ AppendVersionMultipleRoots/StaticFilesAppendVersion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace StaticFilesAppendVersion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHost result = WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>()
+               .Build();
+            return result;
+        }
+    }
+}
