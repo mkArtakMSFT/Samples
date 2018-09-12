22d9243899bee08f9f7d8bb9d649d72676c7b264
Artak Mkrtchyan <artakm@corp.microsoft.com>
2018-09-12 13:18:07 -0700

    Added a new sample showcasing hosting of ASP.NET Core app in a WPF desktop application

---
+++ WpfAspNetCore/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Windows;
+
+namespace WpfAspNetCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateWebHostBuilder(new string[] { }).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
